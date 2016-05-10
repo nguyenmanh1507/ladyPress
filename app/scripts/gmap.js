c6c9a0054afed4969cf2600ab676341a52839d09
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-05-10 16:43:32 +0700

    clean code

--- app/scripts/gmap.js
+++ app/scripts/gmap.js
@@ -1,9 +1,127 @@
+/* eslint-disable no-unused-vars */
 function initMap() {
+  'use strict';
   // Specify features and elements to define styles.
-  const styleArray = [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#0066ff"},{"saturation":74},{"lightness":100}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"},{"weight":0.6},{"saturation":-85},{"lightness":61}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#5f94ff"},{"lightness":26},{"gamma":5.86}]}];
+  const styleArray = [{
+    'featureType': 'administrative',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'off'
+      }
+    ]
+  },
+  {
+    'featureType': 'landscape',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'simplified'
+      },
+      {
+        'hue': '#0066ff'
+      },
+      {
+        'saturation': 74
+      },
+      {
+        'lightness': 100
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'simplified'
+      }
+    ]
+  },
+  {
+    'featureType': 'road',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'simplified'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'off'
+      },
+      {
+        'weight': 0.6
+      },
+      {
+        'saturation': -85
+      },
+      {
+        'lightness': 61
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.arterial',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'off'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.local',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'simplified'
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'simplified'
+      },
+      {
+        'color': '#5f94ff'
+      },
+      {
+        'lightness': 26
+      },
+      {
+        'gamma': 5.86
+      }
+    ]
+  }];
 
   const latLng = {lat: 20.850277, lng: 106.691050},
-        image  = 'https://cdn3.iconfinder.com/data/icons/luchesa-vol-9/128/Lollipop-48.png'
+        image = 'https://cdn3.iconfinder.com/data/icons/luchesa-vol-9/128/Lollipop-48.png'
   ;
 
   // Create a map object and specify the DOM element for display.
@@ -23,3 +141,4 @@ function initMap() {
   });
 
 }
+/* eslint-enable */
